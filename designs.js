522cbffe40513be6cdf5b9b4c441af46eb7e5983
Alex <pickeringacw@gmail.com>
2017-10-06 09:12:47 +0200

    Can now add pixels and in various colours with a dynamically set grid size

--- designs.js
+++ designs.js
@@ -1,10 +1,48 @@
-// Select color input
-// Select size input
+var width = 1;
+var height = 1;
+var colour = '#000';
 
-// When size is submitted by the user, call makeGrid()
+var table = document.getElementById('pixel_canvas');
 
-function makeGrid() {
+document.getElementById('input_width').addEventListener('change', getWidth);
+document.getElementById('input_height').addEventListener('change', getHeight);
+document.getElementById('submit_button').addEventListener('click', makeGrid);
+document.getElementById('colorPicker').addEventListener('change', getColour);
 
-// Your code goes here!
+function getColour(evt) {
+	colour = evt.target.value;
+}
 
+function getWidth(evt) {
+	width = parseInt(evt.target.value);
+}
+
+function getHeight(evt) {
+	height = parseInt(evt.target.value);
+}
+
+function makeGrid(evt) {
+	evt.preventDefault();
+
+	clearGrid();
+
+	for(var j = 0; j < height; j++) {
+		var tr = document.createElement("tr");
+
+		for (var i = 0; i < width; i++) {
+			var td = document.createElement("td");
+			td.setAttribute('onclick', 'test(this)');
+
+			tr.appendChild(td);
+		}
+
+		table.appendChild(tr);
+	}
+}
+
+function clearGrid() {
+}
+
+function test(evt) {
+	evt.style.background = colour;
 }
