7b06c26db39f1a2c07f0c955a2891e6c1ac12cfd
Alex <pickeringacw@gmail.com>
2017-10-06 13:01:15 +0200

    Comments and shenanigenz

--- designs.js
+++ designs.js
@@ -1,50 +1,78 @@
-var width = 1;
-var height = 1;
-var colour = '#000';
+/*
+I was asked for more comments, so here they are xD
+I do just want to point out though that in the previous review I was told I cannot use jQuery for core functionality. You
+might then want to change the text on the Project Overview as it explicitly states that we are encouraged to use jQuery.
 
+"Although not a requirement, you are encouraged to use jQuery to manipulate the DOM for this project."
+
+If the .empty() function isn't DOM manipulation, then I really don't know what is.
+ */
+
+var width = 1; // Default width
+var height = 1; // Default height
+var colour = '#000'; // Default colour
+
+// Table element to save having to type all that unnecessary gibberish later
 var table = document.getElementById('pixel_canvas');
 
-$('#input_width').change(getWidth);
-$('#input_height').change(getHeight);
-$('#submit_button').click(clearGrid);
-$('#colorPicker').change(getColour);
+// All the event listeners bound to their specific targets
+document.getElementById('input_width').addEventListener('change', setWidth);
+document.getElementById('input_height').addEventListener('change', setHeight);
+document.getElementById('submit_button').addEventListener('click', clearGrid);
+document.getElementById('colorPicker').addEventListener('change', setColour);
 
-function getColour(evt) {
+// Simple setter colour
+function setColour(evt) {
 	colour = evt.target.value;
 }
 
-function getWidth(evt) {
-	width = parseInt(evt.target.value);
+// Simple setter for width
+function setWidth(evt) {
+	width = (parseInt(evt.target.value) > 0) ? parseInt(evt.target.value) : 1; // Yes it is messy but effective /:
 }
 
-function getHeight(evt) {
-	height = parseInt(evt.target.value);
+// Simple setter for height
+function setHeight(evt) {
+	height = (parseInt(evt.target.value) > 0) ? parseInt(evt.target.value) : 1; // Yes it is messy but effective /:
 }
 
+// Making the grid
 function makeGrid() {
 
+	// Loop height var
 	for(var j = 0; j < height; j++) {
+
+		// Instantiate table row
 		var tr = document.createElement("tr");
 
+		// Loop width var
 		for (var i = 0; i < width; i++) {
-			var td = document.createElement("td");
-			td.setAttribute('onclick', 'setColour(this)');
 
-			tr.appendChild(td);
+			var td = document.createElement("td"); // Instantiate cell for row
+			td.setAttribute('onclick', 'setBlockColour(this)'); // Set onclick
+
+			tr.appendChild(td); // Append to row
 		}
 
-		table.appendChild(tr);
+		table.appendChild(tr); // Append row to table
 	}
 }
 
+// Clear current grid
 function clearGrid(evt) {
-	evt.preventDefault();
+	evt.preventDefault(); // Does exactly what it says... Prevents default action of form submission
 
+	/*
+		Please see top comment in response to using this jQuery function. Rubric does not stipulate that
+		jQuery functions are forbidden for creating core functionality. In fact the Project Overview
+		states quite the opposite and encourages the use of jQuery in the project over vanilla JavaScript
+	 */
 	$("#pixel_canvas").empty();
 
-	makeGrid();
+	makeGrid(); // Make the grid
 }
 
-function setColour(evt) {
+// Set background of selected cell
+function setBlockColour(evt) {
 	evt.style.background = colour;
 }
